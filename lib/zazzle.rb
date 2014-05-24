630803e0a2ad1a734d9b158477d4fd7247c2256b
daniz <dani.silberstein@gmail.com>
2014-05-24 17:08:06 +0300

    little fixes

--- lib/zazzle.rb
+++ lib/zazzle.rb
@@ -8,6 +8,9 @@ class Zazzle
 	PATH = "api/create/at-%{product_id}"
 
 	def self.get_template_product_api(collage_url, product_id = '238779073096817954')
+		# "#{HOST}?rf=238779073096817954&ax=Linkover&pd=192869976986838675&fwd=ProductPage&ed=true&tc=&ic=&t_collage_iid=#{URI.encode collage_url}"
+		# ap Zazzle.get_template_product_api("http://lorempixel.com/400/400")
+		
 		params = {
 			ax: 'Linkover',
 			rf: product_id,
@@ -22,9 +25,6 @@ class Zazzle
 		path = PATH % {product_id: product_id}
 
 		"#{HOST}/#{path}?#{params.to_query}"
-		# "#{HOST}?rf=238779073096817954&ax=Linkover&pd=192869976986838675&fwd=ProductPage&ed=true&tc=&ic=&t_collage_iid=#{URI.encode collage_url}"
 	end
 
 end
-
-ap Zazzle.get_template_product_api("http://lorempixel.com/400/400")
